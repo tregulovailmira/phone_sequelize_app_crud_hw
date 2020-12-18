a51a7cd6bc7490ced713686e7787dfbef696ac69
Ilmira <tregulovailmira@gmail.com>
2020-12-18 18:13:33 +0200

    Create server

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = require('./router');
+
+const app = express();
+
+app.use(express.json({ type: 'application/vnd.api+json' }));
+
+app.use('/api', router);
+
+app.use((error, req, res, next) => {
+  if (error) {
+    res.status(500).send(error);
+  }
+});
+
+module.exports = app;
